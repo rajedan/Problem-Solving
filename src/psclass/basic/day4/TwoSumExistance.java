77ed51dd746927da36cd19ed65ee07f1a85ff43c
Rajesh Dansena <rajedan@gmail.com>
2018-05-17 22:24:42 +0530

    two sum day4 problem

---
+++ src/psclass/basic/day4/TwoSumExistance.java
@@ -0,0 +1,34 @@
+package psclass.basic.day4;
+
+/**
+ * Given an array of integers, Check if there exist two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * Example:
+ * 
+ * Input : nums = [2, 7, 11, 15], target = 9, Output : True
+ * 
+ * @author rajesh
+ * 
+ */
+public class TwoSumExistance {
+
+	/**
+	 * Checks whether, there exist two elements in the array whose sum is equals
+	 * to target
+	 * 
+	 * @param in
+	 * @param target
+	 * @return true if desired elements exist, false otherwise
+	 */
+	private static boolean twoSum(int[] in, int target) {
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
