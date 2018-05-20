cba05d97650b41f268c25579d50bb9329b725391
Rajesh Dansena <rajedan@gmail.com>
2018-05-20 13:21:54 +0530

    assignment-1 q1-solution

---
+++ src/psclass/basic/assignment1/RemoveDuplicate.java
@@ -0,0 +1,37 @@
+package psclass.basic.assignment1;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ * 
+ * @author rajesh
+ *
+ */
+
+public class RemoveDuplicate {
+	public static int removeDuplicates(int[] in) {
+		 if (in == null || in.length == 0) {
+	            return 0;
+	    }
+		int prev = in[0];
+		int index = 1;
+		for (int i = 1; i < in.length; i++) {
+			if (in[i]!= prev) {
+				in[index] = in[i];
+				index ++;
+				prev = in[i];
+			}
+		}
+		return index;
+	}
+
+	public static void main(String[] args) {
+		int[] in = {};
+		int len = removeDuplicates(in);
+		System.out.println(Arrays.toString(in));
+		for (int i = 0; i < len; i++) {
+			System.out.print(in[i]);
+		}
+	}
+}
