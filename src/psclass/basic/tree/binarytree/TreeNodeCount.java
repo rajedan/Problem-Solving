a54fadbf90352be8dddaa3f40f8adcfa072482b4
Rajesh Dansena <rajedan@gmail.com>
2018-06-04 09:15:09 +0530

    TreeNode Count code draft

---
+++ src/psclass/basic/tree/binarytree/TreeNodeCount.java
@@ -0,0 +1,22 @@
+package psclass.basic.tree.binarytree;
+
+public class TreeNodeCount {
+	/**
+	 * Adhoc strategy
+	 * Time  : O(n)
+	 * Space : O(h)
+	 * where h is height of the Tree
+	 * @param root
+	 * @return
+	 */
+	private static int findTreeNodeCount1(TreeNode root) {
+		int count = 0;
+			
+		return 0;
+	}
+	
+	public static void main(String[] args) {
+		TreeNode root = TreeUtil.getBinaryTree();
+		findTreeNodeCount1(root);
+	}
+}
